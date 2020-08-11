d8d1784f8ac7e53978e024b5c94badc310fc55a1
Flavio Rico Méndez <flavio.rico@fygdevelopers.com>
2020-08-10 19:30:36 -0500

    Incorporación de hover en el menú y estilos para alinear iconos del dropdown

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   nameUser: string = 'Flavio';
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+
   }
 
 }
