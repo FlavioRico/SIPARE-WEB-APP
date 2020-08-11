4008888ec58a5441c053c00de83ac3d9918d7ec3
Flavio Rico Méndez <flavio.rico@fygdevelopers.com>
2020-08-11 16:16:53 -0500

    Lógica inicial para alterar la vista en caso de errores y crear variables necesarias para pintar los valores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BalanceComponent } from './balance/balance.component';
+
+const routes: Routes = [
+  {
+    path: 'balance',
+    component: BalanceComponent
+  },
+  {
+    path: '',
+    redirectTo: '/balance',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
