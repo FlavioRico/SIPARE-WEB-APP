496d4141521993f2760943d84e887a6166a66aa4
Flavio Rico Méndez <flavio.rico@fygdevelopers.com>
2020-08-12 14:02:57 -0500

    Incorporación de renderizado y diseño para cumplir con los casos de consistencia e inconsistencia en los saldos

--- src/app/balance/balance.component.ts
+++ src/app/balance/balance.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/co
 })
 export class BalanceComponent implements OnInit {
 
+  /*Elementos del DOM a renderizar con Renderer2*/
   @ViewChild('iconVerify1') iconComp1: ElementRef;
   @ViewChild('iconVerify2') iconComp2: ElementRef;
   @ViewChild('iconVerify3') iconComp3: ElementRef;
@@ -41,17 +42,15 @@ export class BalanceComponent implements OnInit {
   totalArchivoformated: any;
   totalAuxiliaresformated: any;
 
-  /*Para condicionar el color*/
-  flagBalance = true;
+  constructor(private render: Renderer2) {}
 
-  constructor(private render: Renderer2) {
-
+  ngOnInit(): void {
     this.day = this.fecha.getDate();
     this.month  = this.fecha.getMonth() + 1;
     this.year  = this.fecha.getFullYear();
 
-    this.month = this.format(this.month);
-    this.day = this.format(this.day);
+    this.month = this.formatDate(this.month);
+    this.day = this.formatDate(this.day);
 
     this.fechaCompleta = this.year + '/' + this.month + '/' + this.day;
     this.RCVformated = this.formatTable(this.textRCV, this.valueRCV);
@@ -60,17 +59,13 @@ export class BalanceComponent implements OnInit {
     this.T1ACVformated = this.formatTable(this.textT1ACV, this.valueT1RCV);
     this.totalArchivoformated = this.formatTable(this.textTotal, this.totalArchivo);
     this.totalAuxiliaresformated = this.formatTable(this.textTotal, this.totalAuxiliares);
-    this.addClassVerify(this.flagBalance);
-  }
-
-  ngOnInit(): void {
   }
 
   ngAfterViewInit(){
     this.validaSaldos();
   }
 
-  format(val){
+  formatDate(val){
     if (val < 10) {
       val = '0' + val;
     }
@@ -78,43 +73,40 @@ export class BalanceComponent implements OnInit {
   }
 
   formatTable(textBefore, valTable){
-    return textBefore + valTable;
-  }
-
-  addClassVerify(flagVerify){
-    if(flagVerify){
-      // this.render.addClass(this.iconMod.nativeElement, "icon-verify-ok");
-    }else{
-      console.log('false');
-    }
+    return textBefore + '$' + new Intl.NumberFormat("en-US").format(valTable);
   }
 
   validaSaldos(){
-
     if (this.totalArchivo !== this.totalAuxiliares){
-
       this.render.removeClass(this.messageValidacion.nativeElement, 'alert-success');
       this.render.addClass(this.messageValidacion.nativeElement, 'alert-danger');
-      this.render.setStyle(this.btnAutorizar.nativeElement, 'display', 'none');
+      this.removeElement(this.btnAutorizar);
 
       let iconos = [this.iconComp1, this.iconComp2, this.iconComp3];
-      // tslint:disable-next-line: prefer-for-of
-      // for (let i = 0; i < iconos.length; i ++){
-      //   this.render.removeClass(iconos[i].nativeElement, 'icon-verify-ok');
-      //   this.render.addClass(iconos[i].nativeElement, 'icon-verify-error');
-      // }
+      this.changeIcons(iconos);
 
-      const parent = this.messageValidacion.nativeElement;
-      const child1 = this.iconComp1.nativeElement;
-      const child2 = this.iconComp2.nativeElement;
-      const child3 = this.iconComp3.nativeElement;
-      let arrElements = [child1, child2, child3];
+      this.render.setProperty(this.messageValidacion.nativeElement, 'innerHTML', 'Error. Inconsistencia en saldos del balance.');
+    }
+  }
 
-      // tslint:disable-next-line: prefer-for-of
-      for (let i = 0; i < arrElements.length; i++){
-        this.render.appendChild(parent, arrElements[i]);
-        this.render.removeChild(parent, arrElements[i]);
-      }
+  removeElement(element: ElementRef){
+    this.render.setStyle(element.nativeElement, 'display', 'none');
+  }
+
+  authSucess(element: ElementRef) {
+    this.render.setStyle(element.nativeElement, 'display', 'none');
+    this.render.setProperty(this.messageValidacion.nativeElement, 'innerHTML',
+    `Autorización completada con exito <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-hand-thumbs-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16v-1c.563 0 .901-.272 1.066-.56a.865.865 0 0 0 .121-.416c0-.12-.035-.165-.04-.17l-.354-.354.353-.354c.202-.201.407-.511.505-.804.104-.312.043-.441-.005-.488l-.353-.354.353-.354c.043-.042.105-.14.154-.315.048-.167.075-.37.075-.581 0-.211-.027-.414-.075-.581-.05-.174-.111-.273-.154-.315L12.793 9l.353-.354c.353-.352.373-.713.267-1.02-.122-.35-.396-.593-.571-.652-.653-.217-1.447-.224-2.11-.164a8.907 8.907 0 0 0-1.094.171l-.014.003-.003.001a.5.5 0 0 1-.595-.643 8.34 8.34 0 0 0 .145-4.726c-.03-.111-.128-.215-.288-.255l-.262-.065c-.306-.077-.642.156-.667.518-.075 1.082-.239 2.15-.482 2.85-.174.502-.603 1.268-1.238 1.977-.637.712-1.519 1.41-2.614 1.708-.394.108-.62.396-.62.65v4.002c0 .26.22.515.553.55 1.293.137 1.936.53 2.491.868l.04.025c.27.164.495.296.776.393.277.095.63.163 1.14.163h3.5v1H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/></svg>`);
+  }
+
+  changeIcons(elements: ElementRef[]){
+    console.log(elements);
+    for (let i = 0; i < elements.length; i++) {
+      this.render.removeClass(elements[i].nativeElement, 'icon-verify-ok');
+      this.render.addClass(elements[i].nativeElement, 'icon-verify-error');
+      this.render.setProperty(elements[i].nativeElement, 'innerHTML',
+      '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/> <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/></svg>'
+      );
     }
   }
 
