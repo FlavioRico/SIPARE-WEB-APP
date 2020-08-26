6cc40adb2fef636c8c9b0d7bc232dcf51ff07d5d
Flavio Rico <flavio.rico@fygdevelopers.com>
2020-08-26 15:02:03 -0500

    Refactor navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,38 +6,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  nameUser: string = 'Flavio';
   dependencia: string;
   opcMenu: string;
   opcMenuShort: string;
-
-  @Output() dependenciaClicked: EventEmitter<any> = new EventEmitter();
-
   constructor() {
   }
 
   ngOnInit(): void {
   }
 
-  opcConsarConciliacionCONSAR(){
-    this.dependencia = 'CONSAR';
-    this.opcMenu = 'Conciliación de cifras';
-    this.opcMenuShort = 'Balance';
-    console.log('balance consar');
-    this.dependenciaClicked.emit(this.dependencia);
-  }
-  opcConsarBitacoraCONSAR(){
-    this.dependencia = 'CONSAR';
-    this.opcMenu = 'Bitácora de Archivos';
-    this.opcMenuShort = 'Bitácora';
-    console.log('bitacora consar');
-  }
-  opcConsarConciliacionPROCESAR(){
-    this.dependencia = 'PROCESAR';
-    this.opcMenu = 'Conciliación de cifras';
-    this.opcMenuShort = 'Balance';
-    console.log('balance procesar');
-    this.dependenciaClicked.emit(this.dependencia);
-  }
 }
