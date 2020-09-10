cf9908255e50b29da005dbde30d88256db16dd63
Flavio Rico <flavio.rico@fygdevelopers.com>
2020-09-10 13:48:51 -0500

    Modificación para adaptar diseño y estructura de login responsivo realizado con los servicios del proveedor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication/authentication.service';
+import { Router } from '@angular/router';
+// import { DatepickerOptions } from 'ng2-datepicker';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +10,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  nameUser: string;
-  password: string;
-  route: string;
-  constructor() { }
+  // nameUser: string;
+  // password: string;
+  // route: string;
+
+  public userName : string;
+	public password : string;
+  public errorCode : string;
+  public isError : boolean;
+  public errorMsj : string;
+  public dateRange : any;
+
+  // options: DatepickerOptions = {
+  //   minYear: 2010,
+  //   maxYear: 2030,
+  //   displayFormat: 'MMM D[,] YYYY',
+  //   barTitleFormat: 'MMMM YYYY',
+  //   dayNamesFormat: 'dd',
+  //   firstCalendarDay: 0, // 0 - Sunday, 1 - Monday
+
+  //   minDate: new Date(Date.now()), // Minimal selectable date
+  //   maxDate: new Date(Date.now()),  // Maximal selectable date
+  //   barTitleIfEmpty: '',
+  //   placeholder: '', // HTML input placeholder attribute (default: '')
+  //   addClass: 'form-control', // Optional, value to pass on to [ngClass] on the input field
+  //   addStyle: {}, // Optional, value to pass to [ngStyle] on the input field
+  //   fieldId: 'my-date-picker', // ID to assign to the input field. Defaults to datepicker-<counter>
+  //   useEmptyBarTitle: false, // Defaults to true. If set to false then barTitleIfEmpty will be disregarded and a date will always be shown 
+  // };
+
+  constructor(public authServ : AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
+    localStorage.clear();
   }
 
-  login(){
-
-  }
+  onSubmitLogin(){
+    this.isError= false;
+	  this.authServ.loginService(this.userName, this.password).subscribe(
+      result => {           
+        if(result.resultCode == 0){
+          localStorage.setItem('user', result.nameUser);
+          localStorage.setItem('pass', this.password);
+          localStorage.setItem('username', this.userName);
+          localStorage.setItem('sessionId', result.sessionId);
+          this.router.navigate(['/menu']);
+          this.router.navigate(['/balanceProcesar']);
+        }else if(result.resultCode == 'USER-NOT-FOUND'){
+          this.isError= true;
+          this.errorCode = result.resultCode;
+          this.errorMsj = result.resultDescription;
+          this.userName = '';
+          this.password = '';
+        }
+      },error => {
+        this.router.navigate(['/']);
+          this.userName = '';
+          this.password = '';
+          this.isError= true;
+          this.errorCode = 'ERR-SRV';
+          this.errorMsj = 'Servicio no se encuentra disponible por el momento.';
+      }
+    );
+	}
 
 }
